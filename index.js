d9891b905f77230c5f94ef469f6ccf2882b16d6e
T.M Siddiquee <siddiquee09@gmail.com>
2022-10-29 15:49:53 +0600

    apiloading

--- index.js
+++ index.js
@@ -4,11 +4,31 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 app.use(cors());
 const categories = require("./data/catgories.json");
+const news = require("./data/news.json");
 app.get("/", (req, res) => {
   res.send("News API Running");
 });
 app.get("/categories", (req, res) => {
   res.send(categories);
+});
+
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  if (id === "08") {
+    res.send(news);
+  } else {
+    const categoryNews = news.filter((n) => n.category_id === id);
+    res.send(categoryNews);
+  }
+});
+
+app.get("/news", (req, res) => {
+  res.send(news);
+});
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
 });
 app.listen(port, () => {
   console.log("Server is running on port: ", port);
